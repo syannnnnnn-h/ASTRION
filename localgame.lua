224b8afef46ce7d1d92d5f05b299223a91774fa7
syannnnnnn-h <bilalvnaila@gmail.com>
2025-11-13 08:01:19 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,1 +1,10 @@
-
+return {
+    [82748630433642] = {
+        name = "Mount Yagesya",
+        url = "https://raw.githubusercontent.com/.../KBG.json"
+    },
+    [135285569232987] = {
+        name = "Mount Velora",
+        url = "https://raw.githubusercontent.com/.../velora.json"
+    }
+}
