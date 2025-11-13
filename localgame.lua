5769c55ea688dabd7e0499b2e1ef96fb6aa6ead8
syannnnnnn-h <bilalvnaila@gmail.com>
2025-11-13 08:39:46 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,5 +1,5 @@
 return {
-    [82748630433642] = {
+    [108523862114142] = {
         name = "KOTA BUKAN GUNUNG",
         url = "https://raw.githubusercontent.com/syannnnnnn-h/ASTRION/refs/heads/main/JSON/KBG.json"
     }
