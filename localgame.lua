24f46a448b712a4d54f1a6243ff3ebb54c34ba26
syannnnnnn-h <bilalvnaila@gmail.com>
2025-11-13 08:21:41 +0700

    Update localgame.lua

--- localgame.lua
+++ localgame.lua
@@ -1,10 +1,6 @@
 return {
     [82748630433642] = {
-        name = "Mount Yagesya",
+        name = "KOTA BUKAN GUNUNG",
         url = "https://raw.githubusercontent.com/.../KBG.json"
-    },
-    [135285569232987] = {
-        name = "Mount Veloraa",
-        url = "https://raw.githubusercontent.com/.../velora.json"
     }
 }
